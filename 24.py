ce0fd8a2ac41e9ed71645e856dd0bf7ab11b44c3
Imran Nazar <tf@imrannazar.com>
2021-12-13 09:09:43 +0000

    Some bitfield optimisation

--- 24.py
+++ 24.py
@@ -5,24 +5,43 @@ import sys
 from collections import Counter
 
 def process(content):
+    nodenames = []
+    nodeids = {}
+    nodeid = 0
+    for path in [x.strip() for x in content]:
+        one, two = path.split('-')
+        if one not in nodeids:
+            nodeids[one] = nodeid
+            nodeid += 1
+            nodenames.append(one)
+        if two not in nodeids:
+            nodeids[two] = nodeid
+            nodeid += 1
+            nodenames.append(two)
+
     graph = {}
     for path in [x.strip() for x in content]:
         one, two = path.split('-')
-        graph.setdefault(one, []).append(two)
-        graph.setdefault(two, []).append(one)
+        graph.setdefault(nodeids[one], []).append(nodeids[two])
+        graph.setdefault(nodeids[two], []).append(nodeids[one])
 
     paths = []
-    def walk(node, visited):
+    def walk(node, visited, singlev, doublev):
         nonlocal graph, paths
         visited.append(node)
-        # Dictionary comprehensions, I should use those more
-        overcounts = {n: c for n, c in Counter(visited).items() if n.islower() and len(n) < 3 and c >= 2}
+        if node == nodeids['end']:
+            paths.append(visited)
+            return
+        if nodenames[node].islower():
+            singlev ^= (1 << node)
+            if (singlev & (1 << node)) == 0:
+                doublev |= (1 << node)
         for n in graph[node]:
-            if n.isupper() or (len(n) < 3 and len(overcounts.keys()) == 0) or n not in visited:
-                walk(n, visited.copy())
+            if nodenames[n].isupper() or (len(nodenames[n]) < 3 and doublev == 0) or ((singlev & (1 << n)) == 0 and (doublev & (1 << n)) == 0):
+                walk(n, visited.copy(), singlev, doublev)
         paths.append(visited)
-    walk('start', [])
-    return len([p for p in paths if p[-1] == 'end'])
+    walk(nodeids['start'], [], 0, 0)
+    return len([p for p in paths if p[-1] == nodeids['end']])
 
 def main():
     assert len(sys.argv) == 2, 'Filename not provided'
