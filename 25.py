e3dabfd44780c6c997b4a8badfabaf6fc8c6b6f9
Imran Nazar <tf@imrannazar.com>
2021-12-13 16:04:24 +0000

    Yet more listcomp

--- 25.py
+++ 25.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 # Advent of Code 2021, but I'm learning Python
 # Star 25
-import sys
+import sys, re
 
 def process(content):
     # readlines has an analogue in string terms...
-    points = [[int(p) for p in l.strip().split(',')] for l in content[0].splitlines()]
-    folds = [0 if l.split(' ')[2].split('=')[0] == 'x' else 1 for l in content[1].splitlines()]
-    dims = [max([p[0] for p in points]) + 1, max([p[1] for p in points]) + 1]
+    # This is apparently the syntax for transposing, too
+    points = list(zip(*[[int(p) for p in l.strip().split(',')] for l in content[0].splitlines()]))
+    folds = [0 if re.search(r'(\w)=', l).group(1) == 'x' else 1 for l in content[1].splitlines()]
+    dims = [max(d) + 1 for d in points]
     for f in folds:
         dims[f] = int(dims[f] / 2)
-        for p in range(len(points)):
-            if points[p][f] > dims[f]:
-                points[p][f] -= (points[p][f] - dims[f]) * 2
+        points[f] = [(2 * dims[f] - p) if p > dims[f] else p for p in points[f]]
         break
 
     # We may's well use the deduping of sets
-    return len(list(set([p[1] * dims[0] + p[0] for p in points])))
+    return len(set([p[1] * dims[0] + p[0] for p in zip(*points)]))
 
 def main():
     assert len(sys.argv) == 2, 'Filename not provided'
